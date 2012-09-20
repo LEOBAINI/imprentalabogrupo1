d36a1593067d389f5160893930f8d776d56a1034
bainileonardo@gmail.com <bainileonardo@gmail.com@4e1cc115-d973-e974-556a-d64d4f9a4c9c>
2012-09-20 21:43:40 +0000

    Esta clase imprenta es la interfaz del programa, o sea los requisitos del sistema, mientras más cosas le agreguen más completo queda, sólo necesito los metodos vacíos.

---
+++ Imprenta.java
@@ -0,0 +1,19 @@
+
+public  class Imprenta {
+	
+	
+	public static void llenarOrdenTrabajo(){
+		//completar en base de datos
+		
+	}
+	public static void llenarOrdenCompra(){
+		//completar en base de datos
+	}
+	public static void verEstadoDeTrabajo(OrdenDeTrabajo ot){
+		//consultar con la bd
+	}
+	public static void modificarPrecioIva(double numero){
+		
+	}
+
+}
