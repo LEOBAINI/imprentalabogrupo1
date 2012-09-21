15ee5d6d30db5214d6eddfe6bedd84400ac7b0ea
bainileonardo@gmail.com <bainileonardo@gmail.com@4e1cc115-d973-e974-556a-d64d4f9a4c9c>
2012-09-21 03:12:37 +0000

    ABM de: Clientes,Tareas,marcaPapel,Calidad,FormatoPapel,Variante. Van 6 de 22, Empecé a las 16 hs son las 12:12; 8hs dedicadas.

--- trunk/Imprenta/Prueba.java
+++ trunk/Imprenta/Prueba.java
@@ -1,24 +1,48 @@
 package Imprenta;
 
-
-
 public class Prueba {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ABM abms=new ABM();
-		//Tarea t=new Tarea("Plegadito", "1100");
-		//abms.darAltaTarea(t);
-		//abms.darBajaTarea(5);
-		//abms.modificarTarea(2, t);
-		Iva iva=new Iva(1);
-		//Cliente c=new Cliente("p", "yyyyyy", iva, "dir", "tel", "mail", "nombreCon", "telefotacto", "maacto", "dtraga");
-		//abms.darAltaCliente(c);
-		//abms.darBajaCliente(3);
-		//abms.modificarCliente(4, c);
-		
+		@SuppressWarnings("unused")
+		ABM abms = new ABM();
+		// Tarea t=new Tarea("Plegadito", "1100");
+		// abms.darAltaTarea(t);
+		// abms.darBajaTarea(5);
+		// abms.modificarTarea(2, t);
+		// Iva iva=new Iva(1);
+		// Cliente c=new Cliente("p", "yyyyyy", iva, "dir", "tel", "mail",
+		// "nombreCon", "telefotacto", "maacto", "dtraga");
+		// abms.darAltaCliente(c);
+		// abms.darBajaCliente(3);
+		// abms.modificarCliente(4, c);
+		// abms.darAltAMarcaPapel("papel");
+		// abms.darBajaMarcaPapel(1);
+		// abms.modificarMarcaPapel(2, "papelucho");
+		// abms.darAltaCalidad("alta calidad");
+		// abms.darAltaCalidad("baja calidad");
+		// abms.darAltaCalidad("mediana calidad");
+		// abms.modificarCalidad(1, "masomenos");
+		// abms.modificarCalidad(2, "ta gueno el pelpa");
+		// abms.darBajaCalidad(1);
+		// abms.darAltaFormatoPapel("24", "32");
+		// abms.darAltaFormatoPapel("25", "52");
+		// abms.darAltaFormatoPapel("34", "62");
+		// abms.darBajaFormatoPapel(1);
+		// abms.modificarFormatoPapel(3, 2000, 3000);
+		/*
+		 * abms.darAltaVariante("variante1"); abms.darAltaVariante("variante2");
+		 * abms.darAltaVariante("variante3");
+		 */
+		// abms.modificarVariante(1, "v1");
+		// abms.modificarVariante(2, "v2");
+		// abms.modificarVariante(3, "v3");
+		/*
+		 * abms.darBajaVariante(1); abms.darBajaVariante(2);
+		 * abms.darBajaVariante(3);
+		 */
 
 	}
 
